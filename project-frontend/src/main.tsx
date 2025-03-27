1b5d7fd36dcd097ab2168487386568c7e34fc5e8
nurlybek <orozovnurlybek@gmail.com>
2025-03-27 14:57:02 +0600

    [Frontend] Настроил хранилище Redux, добавил UI-компоненты и axiosApi

--- project-frontend/src/main.tsx
+++ project-frontend/src/main.tsx
@@ -1,6 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import {Provider} from "react-redux";
+import {store} from "./app/store.ts";
+import 'react-toastify/dist/ReactToastify.css'
+import {ToastContainer} from "react-toastify";
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <Provider store={store}>
+        <BrowserRouter>
+            <ToastContainer/>
+            <App />
+        </BrowserRouter>
+    </Provider>
 )
